b60540df13183f217ae7ed33e01df00a7780064c
jonatastalves <jonatas.t.alves@gmail.com>
2022-06-20 12:17:00 -0300

    adicao process env mongo

--- src/config/dbConexao.js
+++ src/config/dbConexao.js
@@ -4,7 +4,7 @@ const DB_USER = 'jonatastalves'
 const DB_PASSWORD = 'torres94'
 
 mongoose
-    .connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@apicluster.piksf3s.mongodb.net/bancoapi?retryWrites=true&w=majority'`)
+    .connect(process.env.MONGODB_URI || `mongodb+srv://${DB_USER}:${DB_PASSWORD}@apicluster.piksf3s.mongodb.net/bancoapi?retryWrites=true&w=majority'`)
 
 let db = mongoose.connection
 
