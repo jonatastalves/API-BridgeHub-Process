012d5be5b46e33a9ec34da8a8a5c70ddfb0b841e
jonatastalves <jonatas.t.alves@gmail.com>
2022-06-20 12:22:59 -0300

    alteracao process.env mongo

--- src/config/dbConexao.js
+++ src/config/dbConexao.js
@@ -2,9 +2,9 @@ import mongoose from "mongoose";
 
 const DB_USER = 'jonatastalves'
 const DB_PASSWORD = 'torres94'
-const MONGODB_URI = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@apicluster.piksf3s.mongodb.net/bancoapi?retryWrites=true&w=majority'`
+
 mongoose
-    .connect(process.env.MONGODB_URI)
+    .connect(process.env.MONGODB_URI=`mongodb+srv://${DB_USER}:${DB_PASSWORD}@apicluster.piksf3s.mongodb.net/bancoapi?retryWrites=true&w=majority'`)
 
 let db = mongoose.connection
 
