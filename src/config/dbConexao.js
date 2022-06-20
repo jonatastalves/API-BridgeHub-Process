dd107abff2f5f09e8b5bbad5733ac1ec21b05204
jonatastalves <jonatas.t.alves@gmail.com>
2022-06-20 12:07:07 -0300

    atualizacao config mongo

--- src/config/dbConexao.js
+++ src/config/dbConexao.js
@@ -4,7 +4,7 @@ const DB_USER = 'jonatastalves'
 const DB_PASSWORD = 'torres94'
 
 mongoose
-    .connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@apicluster.piksf3s.mongodb.net/bancoapi?retryWrites=true&w=majority'`)
+    .connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@apiCluster.piksf3s.mongodb.net/bancoapi?retryWrites=true&w=majority'`)
 
 let db = mongoose.connection
 
