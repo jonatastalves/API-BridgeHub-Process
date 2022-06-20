b726ed1f20be669e72484d2d90820eed6f7b6c67
jonatastalves <jonatas.t.alves@gmail.com>
2022-06-20 11:41:39 -0300

    portas para deploy

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import express from "express";
 import db from "./config/dbConexao.js";
 import RotasUsuario from "./routers/Routers.js";
 
-const port = 3000
+const port = process.env.PORT || 3000
 
 const app = express();
 app.use(express.json());
